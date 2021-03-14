e6733b2e01c7e5881e0b5bd2014fec7973750daf
smaster0517 <>
2021-03-14 19:49:29 +0300

    Add files via upload

--- visualization.py
+++ visualization.py
@@ -20,19 +20,6 @@ def visualize(**images):
     plt.show()
 
 
-def visualize_prediction(file, model, device='cuda', verbose=True, thresh=0.0, n_colors=None):
-    path_to_images = '/content/test_images/test_images'
-    file_path = os.path.join(path_to_images, file, 'images', file + '.png')
-    img = Image.open(file_path)
-    transform_to_tensor = transforms.ToTensor()
-    img_tensor = transform_to_tensor(img).unsqueeze(0)
-    model.to(device)
-    model.eval()
-    with torch.no_grad():
-        predictions = model(img_tensor.to(device))[0]
-    visualize(image=img, mask=predictions.cpu().squeeze(0))
-
-
 def visualize_from_dataset(img):
     img_value, img_boxes = img[0], img[1]["boxes"]
     img_value = img_value.permute(1, 2, 0).numpy()
@@ -55,3 +42,54 @@ def show_image(image, figsize=(16, 9), reverse=True):
     plt.axis('off')
     plt.show()
 
+
+def val_transform(img):
+    to_tensor = transforms.ToTensor()
+    img_tensor = to_tensor(img)
+    return img_tensor.unsqueeze(0)
+
+
+def visualize_prediction_plate(
+    idx,
+    dataset,
+    model,
+    device='cuda',
+    verbose=True,
+    thresh=0.0,
+    n_colors=None,
+    id_to_name=None
+) -> any:
+    filename_to_bbox_dict = dataset.get_data_dict()
+    file_name = list(filename_to_bbox_dict.keys())[idx]
+    file_path = os.path.join('.', 'openimages', dataset.part, file_name + '.jpg')
+
+    img = Image.open(file_path)
+    img_tensor = val_transform(img)
+    model.to(device)
+    model.eval()
+    with torch.no_grad():
+        predictions = model(img_tensor.to(device))
+    prediction = predictions[0]
+
+    if n_colors is None:
+        n_colors = model.roi_heads.box_predictor.cls_score.out_features
+
+    palette = sns.color_palette(None, n_colors)
+
+    img = cv2.imread(file_path, cv2.COLOR_BGR2RGB)
+    h, w = img.shape[:2]
+    image = img
+    for i in range(len(prediction['boxes'])):
+        x_min, y_min, x_max, y_max = map(int, prediction['boxes'][i].tolist())
+        label = int(prediction['labels'][i].cpu())
+        score = float(prediction['scores'][i].cpu())
+        name = id_to_name[label]
+        color = palette[label]
+        if verbose:
+            if score > thresh:
+                print('Class: {}, Confidence: {}'.format(name, score))
+        if score > thresh:
+            image = cv2.rectangle(img, (x_min, y_min), (x_max, y_max), np.array(color) * 255, 2)
+            cv2.putText(image, name, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, np.array(color) * 255, 2)
+    show_image(image)
+    return prediction
